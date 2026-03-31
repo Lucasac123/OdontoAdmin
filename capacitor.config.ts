b1b670cc623314a214f8044a9134d64ed581a66b
OdontoAdmin Developer <dev@odontoadmin.com>
2026-03-31 17:40:07 -0300

    Feat: Add Capacitor and GitHub Actions to build APK

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.odontoadmin.app',
+  appName: 'OdontoAdmin',
+  webDir: 'dist'
+};
+
+export default config;
